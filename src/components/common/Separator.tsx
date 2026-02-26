25c64e8d4c97eb1b37d010bb0e494e06d02a11f0
amirrezash0n <shourvarziamirreza@gmail.com>
2026-02-26 21:14:35 +0330

    Updates

--- src/components/common/Separator.tsx
+++
@@ -1,8 +0,0 @@
-export default function Separator() {
-  return (
-    <div className="relative flex items-center justify-center text-sm text-black-60 my-6">
-      <span className="relative px-4 bg-white-100 z-10">or sign in with</span>
-      <div className="absolute left-0 right-0 h-px bg-black-10"></div>
-    </div>
-  );
-}
